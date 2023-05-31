8b22ab1d2afd4749957e65763361a7c530643016
dunlok <myloc442@gmail.com>
2023-05-31 22:23:18 +0700

    Design form login/register

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,4 +1,79 @@
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Divider, Form, Input } from "antd";
+import classes from "./login.module.css";
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
-  return <div>This is login page</div>;
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <h3 className={classes.title}>Xin chào, ..</h3>
+        <Form
+          name="normal_login"
+          className={classes.loginForm}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Username!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className={classes.button}>
+              Đăng nhập
+            </Button>
+            <Divider orientation="center" plain>
+              {" "}
+              Or{" "}
+            </Divider>
+            <Link to="/register" className={classes.center}>
+              Đăng ký ngay!
+            </Link>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 export default LoginPage;
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,4 +1,79 @@
+import { Button, Divider, Form, Input } from "antd";
+import classes from "./register.module.css";
+
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <h3 className={classes.title}>Đăng ký</h3>
+        <p className={classes.desc}>Nhanh chóng và dễ dàng</p>
+        <Divider />
+        <Form
+          name="basic"
+          labelCol={{ span: 5 }}
+          wrapperCol={{ span: 17 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Họ và tên"
+            name="fullName"
+            rules={[{ required: true, message: "Vui lòng nhập họ tên!" }]}
+          >
+            <Input placeholder="Lê Thị Phương Nga" />
+          </Form.Item>
+
+          <Form.Item
+            label="Gmail"
+            name="email"
+            rules={[
+              { required: true, message: "Vui lòng nhập email!" },
+              {
+                required: false,
+                type: "email",
+                message: "Định dạng email không hợp lệ!",
+              },
+            ]}
+          >
+            <Input placeholder="example@gmail.com" />
+          </Form.Item>
+
+          <Form.Item
+            label="Mật khẩu"
+            name="password"
+            rules={[{ required: true, message: "Vui lòng nhập mật khẩu" }]}
+          >
+            <Input.Password placeholder="@testmatkhau123" />
+          </Form.Item>
+
+          <Form.Item
+            label="Phone"
+            name="phone"
+            rules={[
+              { required: true, message: "Vui lòng nhập số điện thoại!" },
+            ]}
+          >
+            <Input placeholder="091 115 8687" />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 9, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Đăng ký
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 export default Register;
